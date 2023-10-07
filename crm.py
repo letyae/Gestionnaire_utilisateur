5ca413841f830d2205658046d15bfbcb90c1df18
Yae <letekomayao@gmail.com>
2023-10-07 13:13:52 +0100

    2nd features modified

--- crm.py
+++ crm.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-
+import re #regex
+import string 
 class User:
 
     def  __init__(self, first_name: str, last_name: str, phone_number: str="", address: str=""):
@@ -13,17 +14,57 @@ class User:
          return(f"{self.full_name}\n{self.phone_number}\n{self.address}\n")
    
      # ce decorateur permet de dynamiser la fonction
-    #@property
-    #def __attr__(self):
-    #    return (f"{self.first_name} {self.last_name}")
-    
-  #on peut faire sa aussi
     @property
-    def full_name(self):
+    def __attr__(self):
+        return (f"{self.first_name} {self.last_name}")
+    
+  
+    @property
+    def full_name(self)->str:
+       """donne le nom complet
+         Arg: None
+               Return: str
+       """
        return(f"{self.first_name} {self.last_name}")
+    
+    #methode pour avoir representation de classe
+    def __repr__(self) :
+        """indication sur comment recreer objet à partir des info
+               Arg: None
+               Return: object
+         """
+    
+        return f"User({self.first_name}, {self.last_name})"
+    
+    
 
-    #def __repr__(self) -> str:
-    #    return f"User({self.first_name}, {self.last_name})"
+    def _check_phonenumber(self):
+        """ netoyer pour extraire les caratere speciaux des num de tel, et verifier sa validite
+            attr: None
+        """
+        phone_digit=re.sub(r"[+()\s]*","",self.phone_number)
+        if len(phone_digit)<10 or not phone_digit.isdigit():
+            raise ValueError(f"Numéro de telephone {self.phone_number} invalide.")
+        
+    def _check_names(self):
+        """"Verifie la validite du nom et prenom
+        arg: None
+        """
+
+        if not (self.first_name and self.last_name):
+            raise ValueError("Le nom et le prenom  ne peuvent être vide")
+
+        specialcharacter=string.punctuation+string.digits
+        
+        if  any(char in self.full_name for char in specialcharacter):
+            raise ValueError(f"Nom: {self.full_name} invalide.")
+       
+    def _checks(self):
+        """Pour lancer ts les checks"""
+        self._check_phonenumber()
+        self._check_names()
+
+
 
 if __name__=="__main__":
     from faker  import Faker
@@ -36,6 +77,8 @@ if __name__=="__main__":
             phone_number=fake.phone_number(),
             address= fake.address()
             )
-
+        user._checks()
+        print(user.full_name)
         print(user)
+        #print(repr(user))
         print("-"*10,"\n")
